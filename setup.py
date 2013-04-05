32dd4038a255a04a76148b6190d715b21f544d39
Gavin McQuillan <gavin.mcquillan@gmail.com>
2013-04-05 13:30:21 -0700

    Add proper setup.py bits.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+from finial import VERSION
+
+setup(
+    name='django-finial',
+    version='.'.join(map(str, VERSION)),
+    description='Template, Static, and URL Overrides per User.',
+    long_description=open('README.rst').read(),
+    author='Gavin McQuillan',
+    author_email='gavin@urbanairship.com',
+    url='http://github.com/urbanairship/django-finial',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data = { '': ['README.rst'] },
+    install_requires=[
+        'django',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)

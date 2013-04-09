fb5ba55f1a015149605240527e6a5c76d6e38345
Gavin McQuillan <gavin.mcquillan@gmail.com>
2013-04-09 14:50:14 -0700

    Changing to old-school template tag classes; context flummoxed me

--- finial/templatetags/finial_tags.py
+++ finial/templatetags/finial_tags.py
@@ -4,18 +4,30 @@ from django.conf import settings
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True, name='get_media_url')
-def get_media_url(context):
-    """For older generations of Django installs (Pre 1.3).
+class MediaURLNode(template.Node):
 
-    Note: we avoid conflicts with the media Template Context Processor
+    def render(self, context):
+        return context.get('X_MEDIA_URL', settings.MEDIA_URL)
+
+
+class StaticURLNode(template.Node):
+
+    def render(self, context):
+        return context.get('X_STATIC_URL', settings.STATIC_URL)
+
+
+def get_media_url(parser, token):
+    """For older generations of Django installs (Pre 1.3)."""
+    return MediaURLNode()
+
+def get_static_url(parser, token):
+    """If you're using the staticfiles app: use this.
+
+    .. note :: we avoid conflicts with the media Template Context Processor
     by prepending 'X_' to our custom setting.
 
     """
-    return context.get('X_MEDIA_URL', settings.MEDIA_URL)
+    return StaticURLNode()
 
-
-@register.simple_tag(takes_context=True, name='get_static_url')
-def get_static_url(context):
-    """If you're using staticfiles app use this."""
-    return context.get('X_STATIC_URL', settings.STATIC_URL)
+get_media_url = register.tag(get_media_url)
+get_static_url = register.tag(get_static_url)

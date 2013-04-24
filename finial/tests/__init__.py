3487b2615e2c3805da6cf3f4c219a2b3a51c83f9
Gavin McQuillan <gavin.mcquillan@gmail.com>
2013-04-23 17:47:00 -0700

    Remove Template Tag Feature

--- finial/tests/__init__.py
+++ finial/tests/__init__.py
@@ -2,5 +2,4 @@
 # Import our models into the test namepsace so django-admin tests find them.
 from finial.tests.test_middleware import *
 from finial.tests.test_template_context_processor import *
-from finial.tests.test_templatetags import *
 from finial.tests.test_signals import *

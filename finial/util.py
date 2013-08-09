91a4cc5314dcbbb34f533592ea17a1716a22b007
Adam Coddington <me@adamcoddington.net>
2013-08-09 17:10:08 +0000

    Updating decorator to remove model import.

--- finial/util.py
+++ finial/util.py
@@ -1,12 +1,8 @@
-from finial.models import UserTemplateOverride
 
 
 def user_has_override(user, override_name):
     if not user.is_authenticated():
         return False
-    if UserTemplateOverride.objects.filter(
-        user=user,
-        override_name=override_name
-    ).exists():
+    if user.tmpl_overrides.filter(override_name=override_name).exists():
         return True
     return False

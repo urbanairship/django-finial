579a46891dde19d2fa9a88e37192432b042fde69
mmchugh <michael.mchugh@urbanairship.com>
2016-02-17 07:09:04 -0800

    some changes needed for django 1.9 upgrade

--- finial/migrations/0001_initial.py
+++ finial/migrations/0001_initial.py
@@ -1,76 +1,28 @@
-# (c) 2013 Urban Airship and Contributors
-
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'UserTemplateOverride'
-        db.create_table('finial_usertemplateoverride', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(related_name='tmpl_overrides', to=orm['auth.User'])),
-            ('override_name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('override_dir', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('priority', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=1)),
-        ))
-        db.send_create_signal('finial', ['UserTemplateOverride'])
+    initial = True
 
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-    def backwards(self, orm):
-        # Deleting model 'UserTemplateOverride'
-        db.delete_table('finial_usertemplateoverride')
-
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'finial.usertemplateoverride': {
-            'Meta': {'object_name': 'UserTemplateOverride'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'override_dir': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'override_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'priority': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tmpl_overrides'", 'to': "orm['auth.User']"})
-        }
-    }
-
-    complete_apps = ['finial']
+    operations = [
+        migrations.CreateModel(
+            name='UserTemplateOverride',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('override_name', models.CharField(max_length=255)),
+                ('override_dir', models.CharField(max_length=255)),
+                ('priority', models.PositiveSmallIntegerField(default=1)),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='tmpl_overrides', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
